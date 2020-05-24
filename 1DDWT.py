735d8adcdeffa8531b1ed77c0fa972ba7c6c85c9
Rizwan Nato Goulam Aly <rizwan.nato@student-cs.fr>
2020-05-24 13:25:28 +0930

    remove useless recurrence inverse transform done

--- 1DDWT.py
+++ 1DDWT.py
@@ -1,24 +1,41 @@
 import numpy as np
 from math import sqrt
 
-def one_DDWT(input,l = -1):
-    if (l==-1):
-        l = int(len(input) / 2)
-    if(l==0):
-        return input
-    else:
+
+def one_DDWT(input):
+    l = len(input) // 2
+    while l != 0:
         output = np.copy(input)
         for i in range(l):
-            sum = (input[2*i] + input[2*i+1])/sqrt(2)
-            difference = (input[2*i] - input[2*i+1])/sqrt(2)
-            print(sum,difference)
+            sum = (input[2 * i] + input[2 * i + 1]) / sqrt(2)
+            difference = (input[2 * i] - input[2 * i + 1]) / sqrt(2)
             output[i] = sum
-            output[l+i] = difference
-    return one_DDWT(output,int(l/2))
+            output[l + i] = difference
+        input = np.copy(output)
+        l = l // 2
+    return output
 
-input = np.array([1,4,-3,0],dtype=float)
-print(one_DDWT(input))
 
 def inverse_one_DDWT(input):
+    N = int(len(input))
+    l=0
+    while l != N:
+        output = np.copy(input)
+        for i in range(l):
+            sum = input[i] * sqrt(2)
+            difference = input[l+i] * sqrt(2)
+            output[2*i] = (sum + difference)/2
+            output[2*i+1] = (sum - difference)/2
 
-    return input
+        input = np.copy(output)
+        if l == 0:
+            l =1
+        else:
+            l = l * 2
+
+
+    return output
+
+input = np.array([1, 4, -3, 0], dtype=float)
+print(one_DDWT(input))
+print(inverse_one_DDWT(one_DDWT(input)))
